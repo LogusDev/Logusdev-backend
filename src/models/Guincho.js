014c84600503f3d8e9bc920000a9286ce9d6ae04
Murilo Weishaupt <muriloweishaupt@gmail.com>
2025-08-20 19:25:49 -0300

    Criada Model dos Guinchos

---
+++ src/models/Guincho.js
@@ -0,0 +1,57 @@
+import { DataTypes } from "sequelize";
+import Sequelize from "../database/conexao_database.js";
+import Guincheiro from './Guincheiro.js'
+
+const Guincho = Sequelize.define("guincho", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+
+    placa: {
+        type: DataTypes.CHAR(7),
+        allowNull: false
+    },
+
+    marca: {
+        type: DataTypes.STRING(15),
+        allowNull: false
+    },
+
+    modelo: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+
+    ano_fabricacao: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+    capacidade: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+
+    comprimento_plataforma: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    }
+
+}, {
+    timestamps: false
+});
+
+Guincheiro.hasOne(Guincho, {
+    foreignKey: 'guincheiro_id',
+    as: 'guincheiro'
+});
+
+Guincho.belongsTo(Guincheiro, {
+    foreignKey: 'guincheiro_id',
+    as: 'guincheiro'
+});
+
+export default Guincho;
